03c9e82465f896b6ac311a1000d61d1be28cb2f2
Mike Farrow <contact@mikefarrow.co.uk>
2015-04-13 09:41:18 +0100

    adding mpm config

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,9 +1,12 @@
-class server::apache {
+class server::apache (
+	$mpm_module = undef
+) {
 
 	class { '::apache':
 	  default_mods        => false,
 	  default_vhost       => false,
 	  default_confd_files => false,
+	  mpm_module         => $mpm_module,
 	}
 
 	include apache::mod::dir
