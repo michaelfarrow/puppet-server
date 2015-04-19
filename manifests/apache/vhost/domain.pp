6a10775e11e29868efe9aa677c31887dffab0b97
Mike Farrow <contact@mikefarrow.co.uk>
2015-04-19 15:40:47 +0100

    changing docroot

--- manifests/apache/vhost/domain.pp
+++ manifests/apache/vhost/domain.pp
@@ -5,6 +5,7 @@ class server::apache::vhost::domain inherits server::apache::vhost::base {
 	$virtual_docroot = '/var/www/vhosts/%0/current/public'
 
 	Concat::Fragment <| title == "${fqdn}-docroot" |> {
+		docroot => '/var/www',
 		content => template('server/vhost/_docroot.erb'),
 	}
 
