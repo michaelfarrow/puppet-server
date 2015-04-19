09927d209092349ff204cb4170b93881fc135859
Mike Farrow <contact@mikefarrow.co.uk>
2015-04-20 00:00:25 +0100

    using printf instead

--- lib/facter/vhosts.rb
+++ lib/facter/vhosts.rb
@@ -1,11 +1,11 @@
 Facter.add("vhosts") do
 	setcode do
-		Facter::Core::Execution.exec('echo "" -n; for f in $(find /var/www/vhosts/ -mindepth 1 -maxdepth 1 -type d ); do f="${f#/}"; echo "${f##*/}"; done;')
+		Facter::Core::Execution.exec('printf ""; for f in $(find /var/www/vhosts/ -mindepth 1 -maxdepth 1 -type d ); do f="${f#/}"; echo "${f##*/}"; done;')
 	end
 end
 
 Facter.add("vhosts_full") do
 	setcode do
-		Facter::Core::Execution.exec('echo "" -n; for f in $(find /var/www/vhosts/ -mindepth 1 -maxdepth 1 -type d ); do echo "$f"; done;')
+		Facter::Core::Execution.exec('printf ""; for f in $(find /var/www/vhosts/ -mindepth 1 -maxdepth 1 -type d ); do echo "$f"; done;')
 	end
 end
