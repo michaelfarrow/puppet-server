df89a73980d9d11286b238943d77589e6a87dbec
Mike Farrow <contact@mikefarrow.co.uk>
2015-04-19 15:45:51 +0100

    trying something else

--- manifests/apache/vhost/domain.pp
+++ manifests/apache/vhost/domain.pp
@@ -1,11 +1,12 @@
-class server::apache::vhost::domain inherits server::apache::vhost::base {
+class server::apache::vhost::domain inherits server::apache::vhost::base (
+	$docroot = '/var/www/'
+) {
 
 	include apache::mod::vhost_alias
 
 	$virtual_docroot = '/var/www/vhosts/%0/current/public'
 
 	Concat::Fragment <| title == "${fqdn}-docroot" |> {
-		docroot => '/var/www',
 		content => template('server/vhost/_docroot.erb'),
 	}
 
