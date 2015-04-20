1294f0342295839285e40c78d94cc9df08205a74
Mike Farrow <contact@mikefarrow.co.uk>
2015-04-20 11:38:55 +0100

    hushing error

--- lib/facter/vhosts.rb
+++ lib/facter/vhosts.rb
@@ -1,11 +1,11 @@
 Facter.add("vhosts") do
 	setcode do
-		Facter::Core::Execution.exec('printf ""; for f in $(find /var/www/vhosts/ -mindepth 1 -maxdepth 1 -type d ); do f="${f#/}"; echo "${f##*/}"; done;')
+		Facter::Core::Execution.exec('printf ""; if [[ -d /var/www/vhosts/ ]]; then for f in $(find /var/www/vhosts/ -mindepth 1 -maxdepth 1 -type d ); do f="${f#/}"; echo "${f##*/}"; done; fi;')
 	end
 end
 
 Facter.add("vhosts_full") do
 	setcode do
-		Facter::Core::Execution.exec('printf ""; for f in $(find /var/www/vhosts/ -mindepth 1 -maxdepth 1 -type d ); do echo "$f"; done;')
+		Facter::Core::Execution.exec('printf ""; if [[ -d /var/www/vhosts/ ]]; then for f in $(find /var/www/vhosts/ -mindepth 1 -maxdepth 1 -type d ); do echo "$f"; done; fi;')
 	end
 end
