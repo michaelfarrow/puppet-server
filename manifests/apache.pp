21fa1603a61b748c5a3e79f9c9315a7f0d190955
Mike Farrow <contact@mikefarrow.co.uk>
2015-05-06 12:32:33 +0100

    adding correct umask

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -3,10 +3,15 @@ class server::apache (
 ) {
 
 	class { '::apache':
-	  default_mods        => false,
-	  default_vhost       => false,
-	  default_confd_files => false,
-	  mpm_module          => $mpm_module,
+		default_mods        => false,
+		default_vhost       => false,
+		default_confd_files => false,
+		mpm_module          => $mpm_module,
+	} -> 
+
+	file_line { 'Apache umask':
+		path => '/etc/apache2/envvars',
+		line => 'umask 002',
 	}
 
 	include apache::mod::dir
